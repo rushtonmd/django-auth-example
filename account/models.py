b0572b6d2c579780ce3624797bd08e32e5fb1465
Mark D Rushton <mark@rushtonmd.com>
2018-02-01 16:43:29 -0800

    minor commenting in models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# TODOs
+# Create a User Profile model to extend the base User class
+#
+# Reference
+# https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#extending-the-existing-user-model
