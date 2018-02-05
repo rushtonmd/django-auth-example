3a62ada81ee7f6427eeacb5ff507b9edeca4b451
Mark D Rushton <mark@rushtonmd.com>
2018-02-05 09:39:26 -0800

    fix save alert message

--- social/settings.py
+++ social/settings.py
@@ -187,7 +187,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-#STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
