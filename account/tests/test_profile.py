331486f2081b58925118dc4f8e6ed55e77f9d754
Mark D Rushton <mark@rushtonmd.com>
2018-02-06 10:17:27 -0800

    add some photo upload tests

--- account/tests/test_profile.py
+++ account/tests/test_profile.py
@@ -1,6 +1,54 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.uploadedfile import SimpleUploadedFile
+import tempfile
+
+MEDIA_ROOT = tempfile.mkdtemp()
+
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
+class ProfilePhotoUploadTests(TestCase):
+    def _create_image(self):
+        from PIL import Image
+
+        with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as f:
+            image = Image.new('RGB', (200, 200), 'white')
+            image.save(f, 'PNG')
+
+        return open(f.name, mode='rb')
+
+    def setUp(self):
+        # Create a valid User object for use in logging in
+        self.user = User.objects.create_user(username='ProfilePhotoTestsUser', password='123')
+
+        # login
+        self.client.login(username='ProfilePhotoTestsUser', password='123')
+
+        self.image = self._create_image()
+
+    def tearDown(self):
+        self.image.close()
+
+    def test_profile_saved_with_photo(self):
+
+        newuser = User.objects.get(username='ProfilePhotoTestsUser')
+
+        post_response = self.client.post('/account/profile', {'bio': 'bio updated!', 'photo': self.image}, follow=True)
+
+        self.assertEqual(post_response.status_code, 200)
+        self.assertTrue(bool(newuser.profile.photo.name))
+
+    def test_profile_form_errors_with_invalid_image(self):
+        not_an_image = SimpleUploadedFile('front.png', b'this is some text - not an image')
+
+        newuser = User.objects.get(username='ProfilePhotoTestsUser')
+
+        post_response = self.client.post('/account/profile', {'bio': 'bio updated!', 'photo': not_an_image}, follow=True)
+
+        self.assertEqual(post_response.status_code, 200)
+        self.assertFalse(bool(newuser.profile.photo.name))
+        self.assertFormError(post_response, 'form', 'photo', 'Upload a valid image. The file you uploaded was either not an image or a corrupted image.')
 
 
 class ProfileTests(TestCase):
@@ -40,20 +88,10 @@ class ProfileTests(TestCase):
         self.client.login(username='ProfileTestsUser', password='123')
 
         # Create a post to the login page with the correct credentials
-        post_response = self.client.post('/account/profile', {'bio': 'bio updated!'})
+        self.client.post('/account/profile', {'bio': 'bio updated!'})
 
         # Get the newly created user
         bio = User.objects.get(username='ProfileTestsUser').profile.bio
 
         # Assert that the user was found
         self.assertEqual(bio, 'bio updated!')
-
-    # def test_page_loads_correct_template(self):
-    #     self.assertTemplateUsed(self.response, 'account/profile.html')
-
-    # def test_html_elements_exist(self):
-    #     self.assertContains(self.response, '<form', 1)
-    #     self.assertContains(self.response, 'method="post"', 1)
-    #     self.assertContains(self.response, 'type="email"', 1)
-    #     self.assertContains(self.response, 'type="password"', 2)
-    #     self.assertContains(self.response, 'type="submit"', 1)
